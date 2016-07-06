c38b508ae83cbdbd6809821e6c047ea5ec2e4d73
Matolay <matolaypal@gmail.com>
2016-07-06 20:11:13 +0200

    Third data of togcloud OK

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ from db_manage import Dbmanage
 from conditions import *
 
 
-name = input("Please add your database name: ")
-user = input("Please add your user name: ")
-password = input("Please add your password: ")
+# name = input("Please add your database name: ")
+# user = input("Please add your user name: ")
+# password = input("Please add your password: ")
+# obj = Dbmanage(name, user, password)
 
-obj = Dbmanage(name, user, password)
-
-# obj = Dbmanage('postgres', 'postgres', '12345678')
+obj = Dbmanage('palko', 'palko', '12345678')
 
 obj.connect()
 
+
+# FIRST ------------
 all_client_list = obj.runner("""SELECT company_name, COUNT(id) FROM project GROUP BY company_name""")
 
 # print(all_client_list)
 
 
+# SECOND ------------
 usd_currency_list = obj.runner\
     (""" SELECT company_name, budget_value, main_color FROM project WHERE budget_currency = 'USD'""")
 gbp_currency_list = obj.runner\
@@ -29,5 +31,7 @@ eur_currency_list = obj.runner\
 # print(currency_change(eur_currency_list, "eur"))
 
 
-
-
+# THIRD -----------
+company_name_list = obj.runner("""SELECT company_name FROM project GROUP BY company_name""")
+# print(company_name_list)
+print(word_counter(company_name_list))
