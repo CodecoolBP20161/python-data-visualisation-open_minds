7def8a5b28fcb05e047d7b7e6c9bb7b5bd65cb84
Mónika Lombos <lombos.monika@gmail.com>
2016-07-06 17:38:14 +0200

    delete all_clients, create currency_change OK

--- conditions.py
+++ conditions.py
@@ -1,8 +1,20 @@
 from db_manage import Dbmanage
 
 
-def all_clients(obj):
-    client_list = obj.runner("""SELECT company_name, COUNT(id) FROM project GROUP BY company_name""")
-    return client_list
+
+def currency_change(target_list, currency):
+    changed_list = []
+    if currency == "gbp":
+        div = 1.2
+    elif currency == "usd":
+        div = 0.9
+    elif currency == "eur":
+        div = 1.0
+    for element in target_list:
+        element[1] = round(float(element[1])/div, 2)
+        changed_list.append(element)
+    return changed_list
+
+
 
 
