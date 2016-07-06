6c506f3396378f71fb27780f130e46b23a5c0a15
Mónika Lombos <lombos.monika@gmail.com>
2016-07-06 22:18:34 +0200

    manager function list OK

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from conditions import *
 # password = input("Please add your password: ")
 # obj = Dbmanage(name, user, password)
 
-obj = Dbmanage('palko', 'palko', '12345678')
+obj = Dbmanage('postgres', 'postgres', '12345678')
 
 obj.connect()
 
@@ -35,3 +35,10 @@ eur_currency_list = obj.runner\
 company_name_list = obj.runner("""SELECT company_name FROM project GROUP BY company_name""")
 # print(company_name_list)
 print(word_counter(company_name_list))
+
+
+# FOURTH -----------
+manager_list = obj.runner("""SELECT company_name,SUM(status) FROM project GROUP BY company_name""")
+
+# print(manager(manager_list))
+
