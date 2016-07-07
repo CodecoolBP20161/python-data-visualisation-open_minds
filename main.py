822482080bf6da26de29a34ece870df1a2467e53
Matolay <matolaypal@gmail.com>
2016-07-07 10:48:07 +0200

    Ultimate TEST version

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ from conditions import *
 # password = input("Please add your password: ")
 # obj = Dbmanage(name, user, password)
 
-obj = Dbmanage('palko', 'palko', '12345678')
+#ONLY TEST
+obj = Dbmanage("", "", "")
+
 
 obj.connect()
 
 
 # FIRST ------------
 all_client_list = obj.runner("""SELECT company_name, COUNT(id) FROM project GROUP BY company_name""")
-
-# print(all_client_list)
+print(average_colour(all_client_list))
 
 
 # SECOND ------------
@@ -26,26 +27,22 @@ gbp_currency_list = obj.runner\
 eur_currency_list = obj.runner\
     (""" SELECT company_name, budget_value, main_color FROM project WHERE budget_currency = 'EUR'""")
 
-# print(currency_change(usd_currency_list, "usd"))
-# print(currency_change(gbp_currency_list, "gbp"))
-# print(currency_change(eur_currency_list, "eur"))
+print(currency_change(usd_currency_list, "usd"))
+print(currency_change(gbp_currency_list, "gbp"))
+print(currency_change(eur_currency_list, "eur"))
 
 
 # THIRD -----------
 company_name_list = obj.runner("""SELECT company_name FROM project GROUP BY company_name""")
-# print(company_name_list)
-# print(word_counter(company_name_list))
+print(word_counter(company_name_list))
 
 
 # FOURTH -----------
 manager_list = obj.runner("""SELECT company_name,SUM(status) FROM project GROUP BY company_name""")
-
-# print(manager(manager_list))
+print(manager(manager_list))
 
 
 # FIFTH-----------
-
-manager_list = obj.runner("""SELECT manager from project order by manager asc""")
-# print(manager_list)
-# print(first_letter_counter(manager_list))
+manager_letter_list = obj.runner("""SELECT manager from project order by manager asc""")
+print((first_letter_counter(manager_letter_list)))
 
