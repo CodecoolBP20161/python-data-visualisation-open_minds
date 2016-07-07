8daabd6d9fc28eb4bc2c300fb8d773b57ee4ce35
Mónika Lombos <lombos.monika@gmail.com>
2016-07-07 17:34:57 +0200

    finished all requirements

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ from conditions import *
 # obj = Dbmanage(name, user, password)
 
 #ONLY TEST
-obj = Dbmanage("", "", "")
-
-
+obj = Dbmanage("postgres", "postgres", "12345678")
 obj.connect()
 
 
 # FIRST ------------
-all_client_list = obj.runner("""SELECT company_name, COUNT(id) FROM project GROUP BY company_name""")
-print(average_colour(all_client_list))
 
+number_project_list = obj.runner("""SELECT company_name, count(id) from project GROUP BY company_name""")
+# x = get_color(number_project_list)
+# print(three_hex_to_rgb(x))
 
 # SECOND ------------
 usd_currency_list = obj.runner\
@@ -27,22 +26,22 @@ gbp_currency_list = obj.runner\
 eur_currency_list = obj.runner\
     (""" SELECT company_name, budget_value, main_color FROM project WHERE budget_currency = 'EUR'""")
 
-print(currency_change(usd_currency_list, "usd"))
-print(currency_change(gbp_currency_list, "gbp"))
-print(currency_change(eur_currency_list, "eur"))
+# print(currency_change(usd_currency_list, "usd"))
+# print(currency_change(gbp_currency_list, "gbp"))
+# print(currency_change(eur_currency_list, "eur"))
 
 
 # THIRD -----------
 company_name_list = obj.runner("""SELECT company_name FROM project GROUP BY company_name""")
-print(word_counter(company_name_list))
+# print(word_counter(company_name_list))
 
 
 # FOURTH -----------
 manager_list = obj.runner("""SELECT company_name,SUM(status) FROM project GROUP BY company_name""")
-print(manager(manager_list))
+# print(manager(manager_list))
 
 
 # FIFTH-----------
 manager_letter_list = obj.runner("""SELECT manager from project order by manager asc""")
-print((first_letter_counter(manager_letter_list)))
+# print((first_letter_counter(manager_letter_list)))
 
